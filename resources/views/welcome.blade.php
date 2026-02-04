b008128daec6c3bddf3ecfed1bc76811fb7e491c
Irakli <i.botchori@gmail.com>
2026-02-04 16:42:16 +0400

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ninja Network</title>
+</head>
+<body>
+
+    <h1>Welcome to Ninja Network</h1>
+    <p>Your gateway to secure and fast connections.</p>
+
+
+
+    <a href="" class="btn">Find Ninjas</a>
+
+
+
+
+
+
+</body>
+</html>
