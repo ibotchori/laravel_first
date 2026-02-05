e9d460e4c085f462aa95ccc9bb180f7810a77ae2
Irakli <i.botchori@gmail.com>
2026-02-05 13:57:48 +0400

    Adding CSS & Tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,15 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ninja Network</title>
+    @vite('resources/css/app.css')
+
 </head>
-<body>
+<body class="text-center px-8 py-12 ">
 
     <h1>Welcome to Ninja Network</h1>
     <p>Your gateway to secure and fast connections.</p>
 
 
 
-    <a href="/ninjas" class="btn">Find Ninjas</a>
+    <a href="/ninjas" class="btn mt-4 inline-block">Find Ninjas</a>
 
 
 
